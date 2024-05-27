e0e4e796877e8c80299f1da878280b04f8488cc8
pawelek <k.pawelek@vp.pl>
2024-05-27 14:31:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
-function App() {
+export default function App() {
   const [advice, setAdvice] = useState("");
+  const [count, setCount] = useState(0);
+
   const getAdvice = async () => {
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
     setAdvice(data.slip.advice);
+    setCount((c) => c + 1);
   };
+
+  useEffect(() => {
+    getAdvice();
+  }, []);
 
   return (
     <>
       <h1>{advice}</h1>
       <button onClick={getAdvice}>ok</button>
+      <Message count={count} />
     </>
   );
 }
 
-export default App;
+export const Message = (props) => {
+  return <p>{props.count}</p>;
+};
